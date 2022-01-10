ed2c79ff8c6bd2fbf78d34f04b76c9c66ef7fe80
mihaelarotar <rotarandra20@gmail.com>
2022-01-10 17:50:59 +0200

    Completed

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,11 +1,15 @@
 package demo;
 
 import demo.entities.Offerte;
+import demo.entities.Ort;
 import demo.repository.Repository;
 import demo.service.Service;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -25,6 +29,18 @@ public class Main {
                 System.out.println(offer);
             }
             repository.writeToFile("src/offertensortiert.txt", sortedOffers, "&");
+
+            offers = repository.readFromFile("src/offerten.txt", "&");
+            List<Map.Entry<Ort, Float>> top = service.getPlacesTop(offers);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/statistik.txt"));
+            for (Map.Entry<Ort, Float> pair : top) {
+                String line = pair.getKey() + ": " + pair.getValue();
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/demo/service/Service.java
+++ src/demo/service/Service.java
@@ -1,12 +1,18 @@
 package demo.service;
 
 import demo.entities.Offerte;
+import demo.entities.Ort;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Service {
 
+    /**
+     * sorts the given list by price (including tva) descending
+     * @param offers given list
+     * @return the sorted list
+     */
     public List<Offerte> sortOffersByPriceDescending(List<Offerte> offers) {
         for(Offerte offer:offers) {
             offer.setPreis((offer.getPreis()+offer.getPreis()* offer.getMehrwertsteuer()/100));
@@ -15,5 +21,26 @@ public class Service {
         return offers;
     }
 
+    /**
+     * creates a top of places by prices
+     * @param offers the list to be sorted
+     * @return a top of places by prices
+     */
+    public List<Map.Entry<Ort, Float>> getPlacesTop(List<Offerte> offers) {
+        /*Map<Ort, Long> places = offertes.stream()
+                .collect(Collectors.groupingBy(Offerte::getOrt, Collectors.counting()));*/
 
+        Map<Ort, Float> placesMap = new HashMap<>();
+
+        for (Offerte offer : offers) {
+            if(placesMap.containsKey(offer.getOrt())) {
+                placesMap.put(offer.getOrt(), offer.getPreis()+placesMap.get(offer.getOrt()));
+            }
+            else {
+                placesMap.put(offer.getOrt(), offer.getPreis());
+            }
+        }
+
+        return placesMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).toList();
+    }
 }
